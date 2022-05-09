769c0b7e2e66cf5e9c28eadf2729aa6cabb6b2b9
Maykon Sousa <maykon.sousa@hotmail.com>
2022-05-08 22:09:29 -0300

    ajustes web

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { CircleNotch } from "phosphor-react";
 export const Loading = () => {
   return (
     <div className="w-6 h-6 flex items-center justify-center overflow-hidden">
-      <CircleNotch className="w-4 h-4 animate-spin" />
+      <CircleNotch weight="bold" className="w-4 h-4 animate-spin" />
     </div>
   );
 };
--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -2,20 +2,21 @@ import { ChatTeardropDots } from "phosphor-react";
 import { Popover } from "@headlessui/react";
 import { WidgetForm } from "./WidgetForm";
 
-const Widget = () => {
+export const Widget = () => {
   return (
-    <Popover className="absolute bottom-5 right-5 flex flex-col items-end">
+    <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
       <Popover.Panel>
         <WidgetForm />
       </Popover.Panel>
-      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 hover:bg-brand-400 transition-colors text-white flex items-center group">
-        <ChatTeardropDots className="w-6 h-6 " />
+
+      <Popover.Button className="bg-brand-500  px-3 h-12 rounded-[999px] text-white flex items-center group ">
+        <ChatTeardropDots className="w-6 h-6" />
+
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
           Feedback
         </span>
       </Popover.Button>
     </Popover>
   );
 };
-
-export default Widget;
