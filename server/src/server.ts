eea6e62260bed40d92035e2f1ca18a2dedc81d57
Maykon Sousa <maykon.sousa@hotmail.com>
2022-05-11 19:33:08 -0300

    alterando limite de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,8 @@ import "./shared/tsyringe/container"
 import { routes } from './routes';
 
 const app = express();
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({limit: '25mb'}));
 
 app.use(cors());
 app.use(express.json());
